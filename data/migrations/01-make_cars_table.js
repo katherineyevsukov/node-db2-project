474046a803288a1ad9802404dc3ea59476a19db0
Kat <kyevsukov@gmail.com>
2021-12-07 16:33:19 -0500

    created migration

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments('id')
+    tbl.text('vin').unique().notNullable()
+    tbl.text('make').notNullable()
+    tbl.text('model').notNullable()
+    tbl.integer('mileage').notNullable()
+    tbl.text('title')
+    tbl.text('transmission')
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars')
 };
